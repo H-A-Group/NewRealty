8d0f709112ef0da9191c960ccb44433f6e91e808
Your Name <you@example.com>
2022-09-26 18:01:00 +0200

    update startup&login,some widgets

---
+++ lib/presentation/screens/startup.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate/core/utils/app_colors.dart';
+import 'package:real_estate/core/utils/media_query_values.dart';
+import 'package:real_estate/presentation/widgets/signin_buttons.dart';
+import 'package:real_estate/presentation/widgets/start_button.dart';
+
+class StartUp extends StatelessWidget {
+  const StartUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding:  EdgeInsets.only( 
+          right: context.height * 0.03,
+          left: context.height * 0.03
+         ),
+        child: Expanded(
+          child: Column( mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+            Expanded( 
+              child: GridView.count(
+                shrinkWrap: false,
+                crossAxisCount: 2,
+                mainAxisSpacing: context.width*0.02,
+                crossAxisSpacing: context.height*0.02,
+                 children: [
+                  Container(
+                    clipBehavior:Clip.hardEdge ,
+                    width: context.width*0.5,
+                    height: context.height*0.5,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Image.asset("assets/images/login1.png")),
+                    Container(
+                    clipBehavior:Clip.hardEdge ,
+                    width: context.width*0.5,
+                    height: context.height*0.5,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                    child: Image.asset("assets/images/login2.png")),
+                    Container(
+                    clipBehavior:Clip.hardEdge ,
+                   width: context.width*0.5,
+                    height: context.height*0.5,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                    child: Image.asset("assets/images/login3.png")),
+                     Container(
+                    clipBehavior:Clip.hardEdge ,
+                    width: context.width*0.5,
+                    height: context.height*0.5,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                    child: Image.asset("assets/images/splash_image.png",
+                    fit: BoxFit.cover,
+                    
+                    )),
+                  
+                 ],
+              
+              ),
+            ),
+          
+           Row(
+             children: [
+               Text("Ready to ", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: AppColors.darkBlue,
+                fontSize: 25
+               ), ),
+               Text("explore?", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: AppColors.darkBlue,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+                
+               ), ),
+              
+             ],
+           ),
+            SizedBox(height: context.height* 0.03),
+               StartButton(
+                text: "Continue with Email", 
+               width: context.width*0.8, 
+               height: context.height*0.09,
+                onTap: (){},
+              icon: Icons.email,
+                ),
+                
+               SizedBox(height: context.height* 0.01),
+                
+                buildSigninButtons(context),
+                
+                SizedBox(height: context.height*0.001,)
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
---
+++ lib/presentation/widgets/signin_buttons.dart
@@ -0,0 +1,78 @@
+ 
+ import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:real_estate/core/utils/app_colors.dart';
+import 'package:real_estate/core/utils/media_query_values.dart';
+
+Widget buildSigninButtons (BuildContext context)=>  Column(
+  children: [
+    Row( mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: context.height*0.00300,
+                    width: context.width*0.3,
+                    color: AppColors.grey,
+                  
+                  ),
+                   SizedBox(width: context.width*0.02,),
+                  Text("OR",style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: AppColors.grey,
+                  fontSize: 15,
+                 ),),
+                 SizedBox(width: context.width*0.02,),
+                   Container(
+                    height: context.height*0.00300,
+                    width: context.width*0.3,
+                    color: AppColors.grey,
+                  
+                  )
+                ],
+               ),
+                SizedBox(height: context.height* 0.03),
+                Row( mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: (){},
+                      child: Container( 
+                        width: context.width*0.4,
+                        height: context.width*0.2,
+                        decoration: BoxDecoration(
+                          color: AppColors.grey,
+                          borderRadius: BorderRadius.circular(40)
+                        ),
+                      child: Center(child: SvgPicture.asset("assets/images/logo/g.svg")),
+                    
+                      ),
+                    ),
+                    SizedBox(width: context.width*0.03,),
+                    InkWell( 
+                      onTap: (){},
+                      child: Container( 
+                        width: context.width*0.4,
+                        height: context.width*0.2,
+                        decoration: BoxDecoration(
+                          color: AppColors.grey,
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        child: Center(child: SvgPicture.asset("assets/images/logo/f.svg")),
+                      ),
+                    ),
+                  ],
+                ),
+                 SizedBox(height: context.height*0.02,),
+                Row( mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Dont have an account? ",style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: AppColors.darkBlue,
+                    fontSize: 15
+                    ),),
+                    TextButton(onPressed: (){}, child: Text("Register Now",style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: AppColors.darkBlue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+
+                    ),))
+                  ],
+                ),
+  ],
+);
